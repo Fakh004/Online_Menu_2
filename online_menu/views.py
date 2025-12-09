2c8df7620ce620565afe9f3c8063cff7e02bc8d1
fakh <fakhridin008@gmail.com>
2025-12-09 19:58:45 +0300

    edit delete

--- online_menu/views.py
+++ online_menu/views.py
@@ -127,3 +127,29 @@ def dish_list(request):
     return render(request, 'dish_list.html', {'dishes': dishes})
 
 
+
+def dish_edit(request, dish_id):
+    if request.method == 'GET':
+        dish = Dish.objects.get(id=dish_id)
+        return render(request, 'edit_dish.html', {'dish': dish})
+    elif request.method == 'POST':
+        dish = Dish.objects.get(id=dish_id)
+        dish.dish_name = request.POST.get('dish_name', dish.dish_name)
+        dish.description = request.POST.get('description', dish.description)
+        dish.price = request.POST.get('price', dish.price)
+        dish.category = request.POST.get('category', dish.category)
+        if 'image' in request.FILES:
+            dish.image = request.FILES.get('image')
+        dish.save()
+        return redirect('home_name')
+    
+def dish_delete(request, dish_id):
+    dish = Dish.objects.get(id=dish_id)
+    if not dish:
+        return HttpResponse('Dish not found!')
+    if request.method == 'GET':
+        return render(request, 'confirm_delete.html', {'dish': dish})
+    elif request.method == 'POST':
+        dish.delete()
+        return redirect('dish_list')
+    
--- server/urls.py
+++ server/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('dish/<int:dish_id>/', detail_dish, name='detail_dish'),
     path('add_dish/', add_dish, name='add_dish'),
     path('dishes/', dish_list, name='dish_list'),
+    path('dish/edit/<int:dish_id>/', dish_edit, name='dish_edit'),
+    path('dish/delete/<int:dish_id>/', dish_delete, name='dish_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
